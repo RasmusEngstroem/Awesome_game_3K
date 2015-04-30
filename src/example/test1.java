f34beaafc297232354514dd468e093216159c607
MathiasFornitz <mathias.fornitz@gmail.com>
2015-04-30 16:30:53 +0200

    hej

--- src/example/test1.java
+++ src/example/test1.java
@@ -10,6 +10,7 @@ public class test1 {
 	{
 	  public static void main(String[] args)
 	  {
+		  //j
 		  //does my thing work again? 
 	    String backupDir = "/Users/al/backups";
 	     //khajdakjskjhsdjkhasjkahsfkjahaæjdhaældkfhaælkhaælkahælkajdælakdjældk
