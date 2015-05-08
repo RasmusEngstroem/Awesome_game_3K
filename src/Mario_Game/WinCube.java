b0e14018350a63895c1f97fa789f04cfb66d7e8d
MathiasFornitz <mathias.fornitz@gmail.com>
2015-05-08 16:01:46 +0200

    a

--- src/Mario_Game/WinCube.java
+++ src/Mario_Game/WinCube.java
@@ -1,11 +1,29 @@
 package Mario_Game;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class WinCube extends GameEntities  {
 
-	public WinCube(float x_pos, float y_pos) {
+	public Image textureBlock;
+	float x_pos;
+	float y_pos;
+	public Player mario;
+	
+	public WinCube(float x_pos, float y_pos) throws SlickException {
 		super(x_pos, y_pos);
+		textureBlock = new Image("Assets/goal.png");
+		this.x_pos = x_pos;
+		this.y_pos = y_pos;
 		// TODO Auto-generated constructor stub
 	}
 	
-
+	public void update(){
+		
+		checkCollision();
+	}
+	
+	public void render(){}
+	
+	public void checkCollision(){}
 }
