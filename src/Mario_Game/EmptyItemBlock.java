f0285cbff52072b5fbcc6751b148d0327dd8c25b
MathiasFornitz <mathias.fornitz@gmail.com>
2015-05-08 19:55:30 +0200

    aaa

--- src/Mario_Game/EmptyItemBlock.java
+++ src/Mario_Game/EmptyItemBlock.java
@@ -1,6 +1,5 @@
 package Mario_Game;
 
-import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
@@ -8,22 +7,30 @@ import org.newdawn.slick.geom.Rectangle;
 
 public class EmptyItemBlock extends Unbreakable {
 
-	public Image textureBlock;
+	public Image textureBlock; //the image drawn on the box
+	public Rectangle boxShape; //the shape of the block
+	public Rectangle marioHead; //the shape of the head of the player
+	public boolean alive = true; //the state that determines if the block has been hit
+	public boolean placed = false; //is true when the block is placed at the right coordinates
+	public int rep_x; // used to place objects next to this
+	public int rep_y; // used to place objects on top of to this
+	public Player mario; //the player
+	public EmptyItemBlock[] kasse; //an array of the blocks placed next to this
+	public Enemies[] enemy; //an array of all the enemies
+	float x_pos; //the x position of the Blocks
+	float y_pos; //the Y position of the Blocks
 	
-	public Rectangle boxShape;
-	public Rectangle marioHead;
-	public boolean alive = true;
-	public boolean placed = false;
-	public String  t = "kasse";
-	public int rep_x;
-	public int rep_y;
 	
-	public Player mario;
-	public EmptyItemBlock[] kasse;
-	public Enemies[] enemy;
-	float x_pos;
-	float y_pos;
-	
+	/**
+	 * 
+	 * @param x_pos the x position of the Blocks
+	 * @param y_pos the Y position of the Blocks
+	 * @param rep_x the amount of blocks that is placed beside the original Blocks
+	 * @param rep_y the amount of blocks that is placed on top of the original Blocks
+	 * @param mario the player
+	 * @param enemy  an array of all enemies
+	 * @throws SlickException an exception thrown we something goes wrong in the slick library
+	 */
 	public EmptyItemBlock(float x_pos, float y_pos, int rep_x, int rep_y, Player mario, Enemies[] enemy) throws SlickException {
 		super(x_pos, y_pos, rep_x, rep_y);
 		boxShape = new Rectangle(x_pos, y_pos, scaleSize, scaleSize);
@@ -31,12 +38,16 @@ public class EmptyItemBlock extends Unbreakable {
 		marioHead = mario.botBoxT;
 		this.rep_x = rep_x;
 		this.rep_y = rep_y;
-		textureBlock = new Image("Assets/blokEmpty.png");
+		textureBlock = new Image("blokEmpty.png");
 		this.enemy = enemy;
 		placeClones();
-		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * used to update the position of the block
+	 * @param x the x position of the Blocks
+	 * @param y the y position of the Blocks
+	 */
 	public void update(float x, float y){
 		this.x_pos=x-(scaleSize/2);
 		this.y_pos=y-(scaleSize/2);
@@ -51,8 +62,11 @@ public class EmptyItemBlock extends Unbreakable {
 		}
 	}
 	
+	/**
+	 * used to render the visuals of the Block
+	 * @param g the slick graphics
+	 */
 	public void render(Graphics g) {
-		// TODO Auto-generated method stub
 		if(alive){
 			textureBlock.draw(x_pos, y_pos);
 		}
@@ -63,20 +77,19 @@ public class EmptyItemBlock extends Unbreakable {
 		}
 	}
 	
-
-	
+	/**
+	 * checks the collision with the player and the enemies
+	 */
 	public void checkCollision(){
 		if(boxShape.intersects(mario.botBoxT) && placed && alive){
 			mario.collisionU = true;
 		}
-
 		if(boxShape.intersects(mario.botBoxB) && placed && alive){
 			mario.collisionD = true;
 		}
 		if(boxShape.intersects(mario.botBoxL) && placed && alive){
 			mario.collisionL = true;
 		}
-		
 		if(boxShape.intersects(mario.botBoxR) && placed && alive){
 			mario.collisionR = true;
 		}
@@ -84,18 +97,15 @@ public class EmptyItemBlock extends Unbreakable {
 			mario.collisionGT = true;
 		}
 		for(int i = 0; i < enemy.length; i++){
-			
 			if(boxShape.intersects(enemy[i].botBoxB) && placed && alive){
 				enemy[i].collisionB = true;
 			}
-
 			if(boxShape.intersects(enemy[i].botBoxTL) && placed && alive){
 				enemy[i].collisionTL = true;
 			}
 			if(boxShape.intersects(enemy[i].botBoxTR) && placed && alive){
 				enemy[i].collisionTR = true;
 			}
-			
 			if(boxShape.intersects(enemy[i].botBoxL) && placed && alive){
 				enemy[i].collisionL = true;
 			}
@@ -105,10 +115,13 @@ public class EmptyItemBlock extends Unbreakable {
 			if(boxShape.intersects(enemy[i].botBoxGT) && placed && alive){
 				enemy[i].collisionGT = true;
 			}
-			
 		}
 	}
 	
+	/**
+	 * used to place the blocks next to this
+	 * @throws SlickException an exception thrown we something goes wrong in the slick library
+	 */
 	public void placeClones() throws SlickException{
 		if( rep_x != 0){
 			EmptyItemBlock[] kasse = new EmptyItemBlock[rep_x];
