bcb453834f58fcbfb67e467da502cff950277c8c
MathiasFornitz <mathias.fornitz@gmail.com>
2015-05-08 14:44:42 +0200

    aa

---
+++ src/Mario_Game/LevelZone.java
@@ -0,0 +1,38 @@
+package Mario_Game;
+
+import org.newdawn.slick.geom.Rectangle;
+
+public class LevelZone extends GameEntities {
+
+	public float width;
+	public float height;
+	public Rectangle boxShape;
+	public Player mario;
+	
+	
+	public LevelZone(float x_pos, float y_pos, float width, float height, Player mario) {
+		super(x_pos, y_pos);
+		this.width = width;
+		this.height = height;
+		boxShape = new Rectangle(x_pos, y_pos, width, height);
+		this.mario = mario;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void update(float x_pos, float y_pos){
+		this.x_pos = x_pos;
+		this.y_pos = y_pos;
+		
+		boxShape.setCenterX(x_pos-width/2);
+		boxShape.setCenterY(y_pos-height/2);
+		checkIfInsideLevel();
+	}
+	
+	public void checkIfInsideLevel(){
+		if(boxShape.contains(mario.botBoxFB)){
+			mario.alive = false;
+		}
+	}
+	
+
+}
---
+++ src/Mario_Game/WinCube.java
@@ -0,0 +1,11 @@
+package Mario_Game;
+
+public class WinCube extends GameEntities  {
+
+	public WinCube(float x_pos, float y_pos) {
+		super(x_pos, y_pos);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
