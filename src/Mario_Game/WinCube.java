1be08394e6a1f3c9d730cb21b805b112adf9182b
MathiasFornitz <mathias.fornitz@gmail.com>
2015-05-08 16:16:14 +0200

    aaa

--- src/Mario_Game/WinCube.java
+++ src/Mario_Game/WinCube.java
@@ -2,6 +2,7 @@ package Mario_Game;
 
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 
 public class WinCube extends GameEntities  {
 
@@ -9,21 +10,39 @@ public class WinCube extends GameEntities  {
 	float x_pos;
 	float y_pos;
 	public Player mario;
+	public Rectangle boxShape;
+
 	
-	public WinCube(float x_pos, float y_pos) throws SlickException {
+	public WinCube(float x_pos, float y_pos, Player mario) throws SlickException {
 		super(x_pos, y_pos);
+		boxShape = new Rectangle(x_pos, y_pos, scaleSize, scaleSize);
+		this.mario = mario;
 		textureBlock = new Image("Assets/goal.png");
 		this.x_pos = x_pos;
 		this.y_pos = y_pos;
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void update(){
+	public void update(float x_pos, float y_pos){
+		this.x_pos=x_pos-(scaleSize/2);
+		this.y_pos=y_pos-(scaleSize/2);
+		boxShape.setCenterX(x_pos-(scaleSize/2));
+		boxShape.setCenterY(y_pos-(scaleSize/2));
 		
 		checkCollision();
 	}
 	
-	public void render(){}
+	public void render(){
+		
+		textureBlock.draw(x_pos, y_pos);
+		
+	}
 	
-	public void checkCollision(){}
+	public void checkCollision(){
+		
+		if(boxShape.intersects(mario.botBoxFB)){
+			Level1.gameWon = true;
+		}
+		
+	}
 }
