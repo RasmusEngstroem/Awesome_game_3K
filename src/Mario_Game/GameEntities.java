49a5e008109eb69b9804374d9cfff13f3610b9b8
MathiasFornitz <mathias.fornitz@gmail.com>
2015-05-08 19:56:56 +0200

    aaa

--- src/Mario_Game/GameEntities.java
+++ src/Mario_Game/GameEntities.java
@@ -2,9 +2,14 @@ package Mario_Game;
 
 public abstract class GameEntities extends GameObjects{
 	
-	public float x_pos;
-	public float y_pos;
+	public float x_pos; //the x position of the Blocks
+	public float y_pos; //the y position of the Blocks
 
+	/**
+	 * 
+	 * @param x_pos the x position of the Blocks
+	 * @param y_pos the Y position of the Blocks
+	 */
 	public GameEntities(float x_pos, float y_pos) {
 		this.x_pos = x_pos;
 		this.y_pos = y_pos;
