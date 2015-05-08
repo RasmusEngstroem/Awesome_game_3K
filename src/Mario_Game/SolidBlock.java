982c1d2fb7f46c74293e782f6add60b59142f63b
MathiasFornitz <mathias.fornitz@gmail.com>
2015-05-08 11:04:58 +0200

    solid now work

--- src/Mario_Game/SolidBlock.java
+++ src/Mario_Game/SolidBlock.java
@@ -20,10 +20,11 @@ public class SolidBlock extends Unbreakable {
 	
 	public Player mario;
 	public SolidBlock[] kasse;
+	public Enemies[] enemy;
 	float x_pos;
 	float y_pos;
 	
-	public SolidBlock(float x_pos, float y_pos, int rep_x, int rep_y, Player mario) throws SlickException {
+	public SolidBlock(float x_pos, float y_pos, int rep_x, int rep_y, Player mario, Enemies[] enemy) throws SlickException {
 		super(x_pos, y_pos, rep_x, rep_y);
 		boxShape = new Rectangle(x_pos, y_pos, scaleSize, scaleSize);
 		this.mario = mario;
@@ -31,6 +32,7 @@ public class SolidBlock extends Unbreakable {
 		this.rep_x = rep_x;
 		this.rep_y = rep_y;
 		textureBlock = new Image("Assets/blokSolid.png");
+		this.enemy = enemy;
 		placeClones();
 		// TODO Auto-generated constructor stub
 	}
@@ -82,13 +84,37 @@ public class SolidBlock extends Unbreakable {
 		if(boxShape.intersects(mario.botBoxGT) && placed && alive){
 			mario.collisionGT = true;
 		}
+		for(int i = 0; i < enemy.length; i++){
+			
+			if(boxShape.intersects(enemy[i].botBoxB) && placed && alive){
+				enemy[i].collisionB = true;
+			}
+
+			if(boxShape.intersects(enemy[i].botBoxTL) && placed && alive){
+				enemy[i].collisionTL = true;
+			}
+			if(boxShape.intersects(enemy[i].botBoxTR) && placed && alive){
+				enemy[i].collisionTR = true;
+			}
+			
+			if(boxShape.intersects(enemy[i].botBoxL) && placed && alive){
+				enemy[i].collisionL = true;
+			}
+			if(boxShape.intersects(enemy[i].botBoxR) && placed && alive){
+				enemy[i].collisionR = true;
+			}
+			if(boxShape.intersects(enemy[i].botBoxGT) && placed && alive){
+				enemy[i].collisionGT = true;
+			}
+			
+		}
 	}
 	
 	public void placeClones() throws SlickException{
 		if( rep_x != 0){
 			SolidBlock[] kasse = new SolidBlock[rep_x];
 			for(int i=0; i< rep_x; i++){
-				kasse[i]= new SolidBlock(x_pos, x_pos, 0, 0, mario);
+				kasse[i]= new SolidBlock(x_pos, x_pos, 0, 0, mario, enemy);
 			} 
 			this.kasse = kasse;
 		}
